4dabde802e340358b026ca4611ce1e35cf592482
codetech-jr <codetechjunior2000@gmail.com>
2025-06-25 17:20:34 -0400

    cambios en el next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
-import type { NextConfig } from "next";
+// NUEVO next.config.mjs
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // ... otras opciones que tuvieras
 };
 
 export default nextConfig;
